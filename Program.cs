ab82d42a331cc5b39f5b60e08ece0b1eca959bff
Nathan Lubawski <nathanlubawski1@gmail.com>
2022-04-13 19:50:57 -0300

    criado projeto e funcao Soma

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Soma();
+
+        }
+        static void Soma(){
+            
+            Console.Clear();
+            Console.WriteLine("Primeiro valor ");
+            var primeiroValor = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo valor ");
+            var segundoValor = float.Parse(Console.ReadLine());
+            var resultado = primeiroValor + segundoValor;
+
+            Console.WriteLine();
+            Console.WriteLine($"O resultado da soma é {resultado}");
+        }
+    }
+}
